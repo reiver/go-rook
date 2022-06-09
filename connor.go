0ba8f8f7fc59507e9207f03c517264e0dbc7b71a
Charles Iliya Krempeaux <iamreiver@gmail.com>
2022-06-09 10:53:23 -0700

    initial commits

--- connor.go
+++ connor.go
@@ -1,9 +1,16 @@
 package rook
 
+import (
+	"io"
+	"net"
+)
+
+// A Connor represents a lower-level network connection.
+//
+// It is an io.ReadWriteCloser with the usual .Close(), .Read(), and .Write() methods.
+// But also has the .LocalAddr() and .RemoteAddr() methods that are common with Go network programming.
 type Connor interface {
-	Close() error
+	io.ReadWriteCloser
 	LocalAddr() net.Addr
-	Read(b []byte) (n int, err error)
 	RemoteAddr() net.Addr
-	Write(b []byte) (n int, err error)
 }
