180cb7e8a642ef96c6dfc9490f05e0168d80f088
Charles Iliya Krempeaux <iamreiver@gmail.com>
2022-06-10 06:45:48 -0700

    initial commits

---
+++ dlewrite.go
@@ -0,0 +1,41 @@
+package rook
+
+import (
+	"bytes"
+	"io"
+)
+
+const (
+	DLE = byte(0x10)
+	SYN = byte(0x16)
+)
+
+func DLEWrite(w io.Writer, data []byte) (int, error) {
+	if nil == w {
+		return 0, errNilWriter
+	}
+
+	if 0 >= len(data) {
+		return 0, nil
+	}
+
+	var n int
+
+	var buffer bytes.Buffer
+	{
+		for _, b := range data {
+			switch b {
+			case DLE, SYN:
+				buffer.WriteByte(DLE)
+			}
+			buffer.WriteByte(b)
+			n++
+		}
+	}
+
+	{
+		w.Write(buffer.Bytes())
+	}
+
+	return n, nil
+}
